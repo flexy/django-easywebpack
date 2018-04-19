35765d4380d496d2760ac9f9345130e49a6a1c05
Joshua Smith <thewanuki@gmail.com>
2018-04-18 23:42:38 -0700

    Fix broken management commands

--- easywebpack/tests/commands/test_collectstatic.py
+++ easywebpack/tests/commands/test_collectstatic.py
@@ -5,6 +5,26 @@ from easywebpack.management.commands import collectstatic
 
 def test_handle(mocker):
     # Mock and patch
+    super_mock = mocker.Mock()
+    mocker.patch(
+        'easywebpack.management.commands.collectstatic.CollectStaticCommand.handle',  # noqa
+        super_mock,
+    )
+
+    build_webpack_mock = mocker.Mock()
+
+    # Setup
+    command = collectstatic.Command()
+    command.build_webpack = build_webpack_mock
+
+    # Checks
+    command.handle()
+    assert build_webpack_mock.called
+    assert super_mock.called
+
+
+def test_build_webpack(mocker):
+    # Mock and patch
     webpack_build_mock = mocker.Mock()
     mocker.patch(
         'easywebpack.management.commands.collectstatic.webpack_build',
@@ -20,12 +40,12 @@ def test_handle(mocker):
     )
 
     # Setup
-    command = collectstatic.CommandMixin()
+    command = collectstatic.Command()
     command.stdout = stdout_mock
 
     # Debug is False
     settings_mock.DEBUG = False
-    command.handle()
+    command.build_webpack()
     stdout_mock.write.assert_called_with(
         'Building Webpack...',
     )
@@ -33,5 +53,5 @@ def test_handle(mocker):
 
     # Debug is True
     settings_mock.DEBUG = True
-    command.handle()
+    command.build_webpack()
     webpack_build_mock.assert_called_with()
